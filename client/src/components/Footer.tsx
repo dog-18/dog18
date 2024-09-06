50c53ef3df7065a035c57a7060be5a9da95d3b58
sripwoud <me@sripwoud.xyz>
2024-09-06 23:49:48 +0200

    style(client): make Footer bigger

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => (
   <footer className='bg-gray-800 p-4'>
     <div className='container mx-auto text-center'>
       <a
-        className='text-white hover:text-gray-400'
+        className='text-white hover:text-gray-400 text-lg'
         href='https://github.com/dog-18/dog18'
         target='_blank'
         rel='noopener noreferrer'
